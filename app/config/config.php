1aedcb1e5f9d917d6d181c1c4144a22c5a9ba888
CosminCostache <costachecosmyn@gmail.com>
2022-08-04 16:44:00 +0300

    initial commit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'creative-devs');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://localhost/creative-devs');
+// Site Name
+define('SITENAME', 'Creative Devs App');
+// App Version
+define('APPVERSION', '1.0.0');
